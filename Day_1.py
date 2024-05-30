8e4425ccea5083e5c58448215b9ec3460b7cda66
Gul Sher Ali Khan <128397237+GulSherAliKhan@users.noreply.github.com>
2024-05-31 01:16:53 +0500

    Add files via upload
    This is the 1st day of Python where I learn about print statement, input statement len() function, variables and also create a project of band generator where one has to put his/her city name and pet name and it will generate the band name

---
+++ Day_1.py
@@ -0,0 +1,38 @@
+print("Hello World!")
+# #\ is to ignore " " in between string
+# print('She said \"Hello\" and then left')
+# #\n is for new line
+# print("Hello World\n Hello World\n Hello World")
+# # + sign is for adding two strings  concatination
+# print("Hello"+ " "+"GulSher")
+# #input function in Python
+# print("Hello "+ input("What is your name?"))
+#len() function is for getting the lenght of the variable
+#name is the variable and getting input 
+# name=input()
+# #it will print the length of the name of any name
+# print(len(name))
+
+#variables
+#variables can be changed
+# name=input("What is your name?")
+# print(name)
+#===========================
+# swaping  a numbers 
+# a = input()
+# b = input()
+# # 🚨 Don't change the code above ☝️
+# ####################################
+# # Write your code below this line 👇
+# c=a
+# a=b
+# b=c
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+#==========================
+#Day_1 project
+print("Welcome to the band name generator")
+city_name= input("What is the city you grew up in? \n")
+pet_name=input("What is the your pet name? \n")
+print("Your band name is " + city_name +" "+ pet_name)
